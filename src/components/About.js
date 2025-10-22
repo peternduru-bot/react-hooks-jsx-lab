921e476a46a61c0a8050de1465eb8af97f03cc75
jones <pnduru6@gmail.com>
2025-10-22 19:15:21 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../images/demo.png"; // 👈 use the actual file name
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love creating clean, responsive web applications using React.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
